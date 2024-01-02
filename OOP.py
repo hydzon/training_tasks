ce9462bad93c0f93fee459124d87812f39d81f34
Sergey <hydzon@yandex.ru>
2024-01-02 14:34:54 +0900

    Initial Commit

--- OOP.py
+++ OOP.py
@@ -11,37 +11,37 @@ class Predator:
 
 class Cat(Predator):
     def __init__(self, breed, color, age):
-        self.breed = breed
-        self.color = color
-        self.age = age
+        self._breed = breed
+        self._color = color
+        self._age = age
 
     @property
     def breed(self):
-        return self.breed
-    #
-    # @property
-    # def color(self):
-    #     return self.color
-    #
-    # @property
-    # def age(self):
-    #     return self.age
+        return self._breed
+
+    @property
+    def color(self):
+        return self._color
+
+    @property
+    def age(self):
+        return self._age
 
     @breed.setter
     def breed(self, value):
-        self.breed = value
-    #
-    # @color.setter
-    # def color(self, value):
-    #     self.color = value
-    #
-    # @age.setter
-    # def age(self, value):
-    #     if value > self.age:
-    #         self.age = value
-    #     else:
-    #         print("Кошка не может стать моложе")
-    #     return self.age
+        self._breed = value
+
+    @color.setter
+    def color(self, value):
+        self._color = value
+
+    @age.setter
+    def age(self, value):
+        if value > self._age:
+            self._age = value
+        else:
+            print("Кошка не может стать моложе")
+        return self._age
 
     def meow(self):
         print('Мяу!')
@@ -49,6 +49,8 @@ class Cat(Predator):
     def purr(self):
         print('Мрррр')
 
+    def sleep(self):
+        print('Свернулся в клубок и сладко спит.')
 
 """
 Наследование
@@ -57,16 +59,16 @@ class Cat(Predator):
 class HomeCat(Cat):
     def __init__(self, breed, color, age, owner, name):
         super().__init__(breed, color, age)
-        self.owner = owner
-        self.name = name
+        self._owner = owner
+        self._name = name
 
     @property
     def owner(self):
-        return self.owner
+        return self._owner
 
     @property
     def name(self):
-        return self.name
+        return self._name
 
     def getTreat(self):
         print("Мяяяяяяуууууу")
@@ -79,17 +81,30 @@ class HomeCat(Cat):
     def name(self, value):
         self._name = value
 
+"""
+Полиморфизм
+"""
+class Parrot:
+  def sleep(self):
+    print('Сел на жёрдочку и уснул.')
+
+
+def homeSleep(animals):
+    animals.sleep()
+
 
 # cat1 = Cat("Британец", "бежевый", 1)
 cat1 = Cat("Британец", "бежевый", 1)
 cat1.age = 2
-
 cat2 = HomeCat("Мейкун", "серый", 3, "Михаил", "Бука")
+pat = Parrot()
+
 
 cat1.hunt()
 print(cat1.breed)
 print(id(cat1))
-
+homeSleep(cat1)
+homeSleep(pat)
 
 cat2.hunt()
 print(cat2.breed)
