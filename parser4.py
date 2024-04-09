45769c3286b22c6c03905a2fc8b6ab6b306a2c0c
Sergey <hydzon@yandex.ru>
2024-04-09 20:55:49 +0900

    update

--- parser4.py
+++ parser4.py
@@ -4,16 +4,28 @@ import aiohttp
 from bs4 import BeautifulSoup
 from pathlib import Path
 import requests
+import time
 
 
 auto_catalog = {}
 auto_logos = {}
-
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36'
 }
+page_counter = 0
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+async def counter(foo):
+    async def wrapper(*args, **kwargs):
+        global page_counter
+        await foo(*args, **kwargs)
+        page_counter -= 1
+        print(f'Осталось:{page_counter}')
+        return foo
+    return wrapper
 
 
 def get_response(url):
@@ -27,25 +39,44 @@ def save_page(url, file_path):
         file.write(source.text)
 
 
-async def async_save_page(url, file_path):
-    ...
+async def async_save_page(url, file_path, session):
+    async with session.get(url) as response:
+        if response.status != 200:
+            print(response.status)
+        else:
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(await response.text(encoding='UTF-8'))
+
+
+async def async_save_logos(url):
+    await asyncio.sleep(1)
+    source = get_response(url)
+    with open(f'AutoCatalog/', 'w', encoding='utf-8') as file:
+        file.write(source.text)
+    Path(f'AutoCatalog/{url.text}').mkdir(parents=True, exist_ok=True)
+    # auto_logos[name] = {
+    #                 'Link': link
+    #             }
 
 
 async def async_parsing():
     tasks = []
-
+    global page_counter
     async with aiohttp.ClientSession() as session:
         for i in range(4):
             with open(f'car_logos_page{i+1}.html', 'r', encoding='utf-8') as file:
                 source = file.read()
             soup = BeautifulSoup(source, 'lxml')
             for el in soup.find_all(class_='meta-image'):
-                auto_logos[el.find('a').get('title')] = {
-                    'Link': el.find('a').get('href')
-                }
-
+                name = el.find('a').get('title')
+                tasks.append(async_save_page(el.find('a').get('href'),
+                                             f'AutoCatalog/_AutoLogos_/{name}.html',
+                                             session))
+        page_counter = len(tasks)
+        start = time.time()
+        print(f'Старт сохранения страниц...')
         await asyncio.gather(*tasks)
-    print(auto_logos)
+        print(f'Сохранено за {time.time() - start:.2f}с')
 
 
 def sync_parsing():
